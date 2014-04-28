1de21f619f706dbbd67f999df139c8b9b61b43f8
PauliusNeverbickas <pnever12@student.aau.dk>
2014-04-28 18:07:16 +0200

    a script for sounds
    Its the script where one button is working (making sound) ;

---
+++ PlayPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+
+namespace Sansula_Kalimba
+{
+	[Activity (Label = "PlayPage")]			
+	public class PlayPage : Activity
+	{
+		MediaPlayer _player;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Play);
+
+			_player = MediaPlayer.Create(this, Resource.Raw.sound1);
+
+			var sound1 = FindViewById<Button> (Resource.Id.sound1);
+
+			sound1.Click += delegate {
+				sound1.Text = string.Format ("asdasd");
+				_player.Start();
+			};
+			//	_player = MediaPlayer.Create(this, Resource.Raw.sound1);
+
+		
+
+
+
+		}
+	}
+}
+
